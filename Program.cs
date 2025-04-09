d2d66f6100338cba7fc43a7608e1ba1f0c0bf7bd
Jan Leirvaag <lja022025@gmail.com>
2025-04-09 08:56:34 +0200

    update

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@ namespace Overload_try_except;
 
 class Program
 {
-    //create a Calculator class    
+    //Create a Calculator class    
     public class Calculator
     {
-        //since we aren't using any custom logic in get/set we'll use C#'s auto-implementation        
+        //Since we aren't using any custom logic in get/set we'll use C#'s auto-implementation        
         public string? Number1 { get; set; }
         public string? Number2 { get; set; }
         public string? Number3 { get; set; }
 
-        //create method's with the same name but a different amount or type of parameters == method overload.
-        //but if the int version of Add only had two integer parameters it would have been skipped
+        //Create method's with the same name but a different amount or type of parameters == method overload.
+        //But if the int version of Add only had two integer parameters it would have been skipped
         //since C# implicitly converts int to decimal types and so would have used the double version
         public int Add(int a, int b, int c)
         {
@@ -24,27 +24,27 @@ class Program
         {
             return a + b;
         }
-    }//end of class Calculator    
+    }//End of class Calculator    
 
-    //method needs to be static so we can access it in Main
+    //Method needs to be static so we can access it in Main
     static private void ReadInput()
     {
         bool programRunning = true;
 
-        //instantiate our Calc object.
-        //we could do this at a higher level (Program class) but it's not really
+        //Instantiate our Calc object.
+        //We could do this at a higher level (Program class) but it's not really
         //necessary since we only use it in the ReadInput method
         Calculator calc = new Calculator();
 
-        //fetch local decimal separator for numbers
+        //Fetch local decimal separator for numbers
         string decimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
 
-        int num1 = 0;
-        int num2 = 0;
+        int num1;
+        int num2;
         int num3;
         double decNum1;
         double decNum2;
-        const string decimalMessage = "is a whole number. Decimal expected";
+        const string decimalMessage = "is a whole number, decimal expected";
         const int dontPanic = 42;
         const string dontPanicMessage = @"
       ____                     __   __        ____                               
@@ -57,7 +57,7 @@ class Program
                                                                                  ";
 
 
-        //the while loop will continue until the user chooses option 3 (exit) since the loop depends on
+        //The while loop will continue until the user chooses option 3 (exit) since the loop depends on
         //programRunning being true.
         while (programRunning)
         {
@@ -67,7 +67,7 @@ class Program
             Console.WriteLine("3. Exit");
             string? input = Console.ReadLine();
 
-            //choose case based on the main menu choices the user types in (1,2,3)
+            //Choose case based on the main menu choices the user types in (1,2,3)
             switch (input)
             {
                 case "1":
@@ -80,22 +80,22 @@ class Program
 
                     try
                     {
-                        //convert strings to integer in a try/catch block
-                        //will throw Exception if conversion fails
+                        //Convert strings to integer in a try/catch block.
+                        //Will throw Exception if conversion fails
                         num1 = Convert.ToInt32(calc.Number1);
                         num2 = Convert.ToInt32(calc.Number2);
                         num3 = Convert.ToInt32(calc.Number3);
 
-                        //display a special message if the sum is the answer to Life, the Universe & Everything :-)
+                        //Display a special message if the sum is the answer to Life, the Universe & Everything :-)
                         if (calc.Add(num1, num2, num3) == dontPanic)
                             Console.WriteLine(dontPanicMessage);
                         else
-                            //display the sum                            
+                            //Display the sum                            
                             Console.WriteLine($"The sum is: {calc.Add(num1, num2, num3)}\n");
                     }
                     catch (Exception error)
                     {
-                        //show Exception error message
+                        //Show Exception error message
                         Console.WriteLine($"{error.Message}\n");
                     }
                     break;
@@ -108,14 +108,14 @@ class Program
 
                     try
                     {
-                        //convert strings to decimal in a try/catch block
-                        //will throw Exception if conversion fails.
-                        //since C# implicitly converts int to decimal types, 
+                        //Convert strings to decimal in a try/catch block.
+                        //Will throw Exception if conversion fails.
+                        //Since C# implicitly converts int to decimal types, 
                         //pure integer's like 5 will convert as well 
                         decNum1 = Convert.ToDouble(calc.Number1);
                         decNum2 = Convert.ToDouble(calc.Number2);
 
-                        //but we will check if userinput does not contain a dot, i.e. it's a decimal type
+                        //But we will check if userinput does not contain a dot, i.e. it's not a decimal type
                         //and inform user that a decimal is expected
                         if (!calc.Number1.Contains(decimalSeparator) && !calc.Number2.Contains(decimalSeparator))
                         {
@@ -131,11 +131,11 @@ class Program
                             Console.WriteLine($"'{calc.Number2}' {decimalMessage}.\n");
                         }
 
-                        //display a special message if the sum is the answer to Life, the Universe & Everything :-)
+                        //Display a special message if the sum is the answer to Life, the Universe & Everything :-)
                         if (calc.Add(decNum1, decNum2) == dontPanic)
                             Console.WriteLine(dontPanicMessage);
                         else
-                            //display the sum
+                            //Display the sum
                             Console.WriteLine($"The sum is: {calc.Add(decNum1, decNum2)}\n");
                     }
                     catch (Exception error)
@@ -145,13 +145,13 @@ class Program
                     break;
 
                 case "3":
-                    //if user chooses 3, set programRunning to false ensuring that the while loop
+                    //If user chooses 3, set programRunning to false ensuring that the while loop
                     //will exit
                     programRunning = false;
                     break;
 
                 default:
-                    //inform user that he must choose 1,2 or 3                    
+                    //Inform user that he must choose 1,2 or 3                    
                     Console.WriteLine("Please choose '1','2' or '3'.\n");
                     break;
             }
@@ -160,9 +160,9 @@ class Program
 
     static void Main(string[] args)
     {
-        //call method
+        //Call method
         ReadInput();
 
-    }//end of Main
+    }//End of Main
 }
 
